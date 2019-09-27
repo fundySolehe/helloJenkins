d746a5b4841614c747b903c76a5368337a4942ff
FundySolehe <the-fundy@hotmail.com>
2019-09-27 14:57:11 +0700

    First commit

---
+++ src/main/java/test/jenkins/hello/HelloRestController.java
@@ -0,0 +1,14 @@
+package test.jenkins.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloRestController {
+
+	@GetMapping(value = "/sayHello")
+	public String sayHello() {
+		return "HELLO madafaka";
+	}
+}
